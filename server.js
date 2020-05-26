57c0a08f6f6b7cc6ef7d09963ac69edde0df1b1b
Tung Huynh <htqtung@gmail.com>
2020-05-26 15:40:17 +0300

    added database connection to Heroku Postgres

--- server.js
+++ server.js
@@ -17,10 +17,8 @@ app.use(cors());
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'tunghuynh',
-    password: '',
-    database: 'smart-brain',
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
